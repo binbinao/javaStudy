3295314b4d930933d409e392bdf108d9924891d3
binbinao <duobinji@gmail.com>
2020-02-14 15:25:39 +0800

    update

---
+++ MethodDemo.java
@@ -0,0 +1,9 @@
+public class MethodDemo{
+    public static void main(String[] args){
+        method();
+    }
+
+    public static void method(){
+        System.out.println("Method is called.");
+    }
+}
