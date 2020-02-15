6403145bffc8e067de2d45202d81d489289a95bf
binbinao <duobinji@gmail.com>
2020-02-15 11:06:43 +0800

    update

---
+++ StringUtil.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class StringUtil {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        //获取String值
+        String a=in.nextLine();
+        StringBuilder stringBuilder = new StringBuilder(a);
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (stringBuilder.charAt(i)==' ') {
+                System.out.println(i);
+                stringBuilder.deleteCharAt(i);
+                i--;
+            }else {
+                stringBuilder.charAt(i);
+            }
+        }
+        System.out.println(stringBuilder.toString());
+    }
+}
