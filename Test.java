c7f8d90562fb10910bc2e9ff9bd9c21016d8c803
binbinao <duobinji@gmail.com>
2020-02-21 15:56:47 +0800

    update

--- Test.java
+++ Test.java
@@ -1,17 +1,8 @@
 public class Test{
     public static void main(String[] args){
-        int[] a1={1,2,3};
-        int[] a2;
+        Dog a = new Dog();
 
-        a2=a1;
-
-        for(int i=0; i<a2.length; i++){
-            a2[i]++;
-        }
-        for(int i=0; i<a1.length; i++){
-            System.out.println(a1[i]);
-        }
+        a.legNum = 4;
+        a.bark();
     }
 }
-
-//好大一个坑啊，数组赋值，居数组相等了。
