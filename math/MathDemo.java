62506eff3d30c92580d8b14c38e23323339522e9
binbinao <duobinji@gmail.com>
2020-02-24 10:47:34 +0800

    update

---
+++ math/MathDemo.java
@@ -0,0 +1,10 @@
+public class MathDemo{
+    public static void main(String[] args){
+        System.out.println(Math.abs(-12.7));
+        System.out.println(Math.ceil(12.7));
+        System.out.println(Math.rint(12.4));
+        System.out.println("sin30 = "+Math.sin(Math.PI/6));
+        System.out.println("cos30 = "+Math.cos(Math.PI/6));
+        System.out.println("tan30 = "+Math.tan(Math.PI/6));
+    }
+}
---
+++ math/MathTest.java
@@ -0,0 +1,11 @@
+public class MathTest{
+    public static void main(String[] args){
+        double a, b;
+        a = Math.random();
+        b = Math.random();
+
+        System.out.println(a+"  "+b);
+
+        System.out.println(Math.max(a, b));
+    }
+}
